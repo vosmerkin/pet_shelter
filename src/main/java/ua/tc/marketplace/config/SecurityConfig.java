6b43903a2289b213c5cbd7ae213d9995b3047dc0
Yevhen Melnyk <vosmerkin.evgen@gmail.com>
2024-12-03 15:27:17 +0200

    Fix securityconfig

--- src/main/java/ua/tc/marketplace/config/SecurityConfig.java
+++ src/main/java/ua/tc/marketplace/config/SecurityConfig.java
@@ -93,6 +93,7 @@ public class SecurityConfig {
                         "/api/v1/tag",
                         "/api/v1/tag/{id}",
                         "/api/v1/user/{id}",
+                        "/api/v1/category",
                         "/api/v1/category/counted")
                     .permitAll()
                     .anyRequest()
