2b2c15944e06d059064aefde41b0b3e8a75c0298
Yevhen Melnyk <vosmerkin.evgen@gmail.com>
2025-06-03 22:52:05 +0300

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#mvn clean compile test package
+
+echo copy files to suse
+scp target/marketplace-0.0.1-SNAPSHOT.jar evgen@192.168.31.61:/home/evgen/pet_shelter
+
+echo setting env variables
+
+
+echo docker postgress
+#docker compose up -d
+#if [ ! "$(docker ps -a -q -f name=postgres_db_marketplace)" ]; then
+#    if [ "$(docker ps -aq -f status=exited -f name=postgres_db_marketplace)" ]; then
+#        # cleanup
+#        docker rm postgres_db_marketplace
+#    fi
+#    # run your container
+#    docker run -d --name <name> my-docker-image
+#fi
