feec4a98010fbab001d809f6fb0302ba87058b77
Yevhen Melnyk <vosmerkin.evgen@gmail.com>
2025-06-26 17:41:12 +0300

    adding adAttributes to ad

--- src/main/java/ua/tc/marketplace/util/SampleDataService.java
+++ src/main/java/ua/tc/marketplace/util/SampleDataService.java
@@ -15,87 +15,101 @@ import ua.tc.marketplace.service.UserService;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Locale;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class SampleDataService {
 
-    private List<Long> userIds;
-    private List<Long> categoryIds;
-    private final UserRepository userRepository;
-    private final CategoryRepository categoryRepository;
-    private final AttributeRepository attributeRepository;
-//    private final AdAttribute a;
-    private final UserService userService;
-    private final CategoryService categoryService;
-    private final Faker faker = new Faker(new Locale("en"));
+  private List<Long> userIds;
+  private List<Long> categoryIds;
+  private List<Attribute> attributes;
+  private final UserRepository userRepository;
+  private final CategoryRepository categoryRepository;
+  private final AttributeRepository attributeRepository;
+  //    private final AdAttribute a;
+  private final UserService userService;
+  private final CategoryService categoryService;
+  private final Faker faker = new Faker(new Locale("en"));
 
-    private  void setUserIds(){
-        if (userIds==null){
-            userIds = userRepository.findAll().stream()
-                    .map(User::getId)
-                    .toList();
-        }
+  private void setUserIds() {
+    if (userIds == null) {
+      userIds = userRepository.findAll().stream().map(User::getId).toList();
     }
+  }
 
-    private void setCategoryIds(){
-        if (categoryIds==null){
-            categoryIds=categoryRepository.findAll().stream()
-                    .map(Category::getId)
-                    .toList();
-        }
+  private void setCategoryIds() {
+    if (categoryIds == null) {
+      categoryIds = categoryRepository.findAll().stream().map(Category::getId).toList();
     }
+  }
 
-
-    public void addSampleUsers(int count) {
-        for (int i = 0; i < count; i++) {
-            User user = User.builder()
-                    .email(faker.internet().emailAddress())
-                    .password(faker.internet().password(8, 16)) // generate dummy password
-                    .userRole(faker.options().option(UserRole.class))
-                    .firstName(faker.name().firstName())
-                    .lastName(faker.name().lastName())
-                    .enabled(true)
-                    // Optional fields like profilePicture, contactInfo, location can be added if needed
-                    .build();
-
-            userRepository.save(user);
-        }
-        System.out.println("✅ Added " + count + " sample users to the database.");
+  private void setAttributeIds() {
+    if (attributes == null) {
+      attributes = attributeRepository.findAll();
     }
+  }
 
-    private void addSampleCategories(){
-        categoryRepository.save(new Category(null,"dog",null) );
-        categoryRepository.save(new Category(null,"cat",null) );
-        categoryRepository.save(new Category(null,"rodent",null) );
-        categoryRepository.save(new Category(null,"bird",null) );
-        categoryRepository.save(new Category(null,"fish",null) );
-        categoryRepository.save(new Category(null,"other",null) );
-    }
-    private void addSampleAttributes(){
- attributeRepository.save(new Attribute( null,"breed" , ValueType.STRING));
-        ('breed', 'STRING'),
-        ('age', 'STRING'),
-        ('size', 'STRING'),
-        ('gender', 'STRING'),
-        ('coat_length', 'STRING'),
-        ('color', 'STRING'),
-        ('health_condition', 'STRING'),
-        ('pet_name', 'STRING');
-    }
+  public void addSampleData() {
+    addSampleUsers(100);
+    addSampleAttributes();
+    addSampleCategories();
 
-//    public void addSampleAds(int count){
-//        setUserIds();
-//        setCategoryIds();
-//
-//        List<AdAttribute> adAttributes =
-//        Ad ad= Ad.builder()
-//                .author(userService.findUserById(faker.options().nextElement(userIds)))
-//                .title(faker.animal().name())
-//                .description(faker.lorem().sentence())
-//                .price(BigDecimal.valueOf(faker.random().nextDouble()))
-//                .category(categoryService.findCategoryById(faker.options().nextElement(categoryIds)))
-//                        .              build();
-//    }
+    addSampleAds(1000);
+  }
+
+  public void addSampleUsers(int count) {
+    for (int i = 0; i < count; i++) {
+      User user =
+          User.builder()
+              .email(faker.internet().emailAddress())
+              .password(faker.internet().password(8, 16)) // generate dummy password
+              .userRole(faker.options().option(UserRole.class))
+              .firstName(faker.name().firstName())
+              .lastName(faker.name().lastName())
+              .enabled(true)
+              // Optional fields like profilePicture, contactInfo, location can be added if needed
+              .build();
+
+      userRepository.save(user);
+    }
+    System.out.println("✅ Added " + count + " sample users to the database.");
+  }
+
+  private void addSampleCategories() {
+    setAttributeIds();
+    categoryRepository.save(new Category(null, "dog", attributes));
+    categoryRepository.save(new Category(null, "cat", attributes));
+    categoryRepository.save(new Category(null, "rodent", attributes));
+    categoryRepository.save(new Category(null, "bird", attributes));
+    categoryRepository.save(new Category(null, "fish", attributes));
+    categoryRepository.save(new Category(null, "other", attributes));
+  }
+
+  private void addSampleAttributes() {
+    attributeRepository.save(new Attribute(null, "breed", ValueType.STRING));
+    attributeRepository.save(new Attribute(null, "age", ValueType.STRING));
+    attributeRepository.save(new Attribute(null, "size", ValueType.STRING));
+    attributeRepository.save(new Attribute(null, "gender", ValueType.STRING));
+    attributeRepository.save(new Attribute(null, "coat_length", ValueType.STRING));
+    attributeRepository.save(new Attribute(null, "color", ValueType.STRING));
+    attributeRepository.save(new Attribute(null, "health_condition", ValueType.STRING));
+    attributeRepository.save(new Attribute(null, "pet_name", ValueType.STRING));
+  }
+
+  public void addSampleAds(int count) {
+    setUserIds();
+    setCategoryIds();
+
+    //          List<AdAttribute> adAttributes =
+    Ad ad =
+        Ad.builder()
+            .author(userService.findUserById(faker.options().nextElement(userIds)))
+            .title(faker.animal().name())
+            .description(faker.lorem().sentence())
+            .price(BigDecimal.valueOf(faker.random().nextDouble()))
+            .category(categoryService.findCategoryById(faker.options().nextElement(categoryIds)))
+            .build();
+      //          List<AdAttribute> adAttributes=
+      AdAttribute adAttribute= new AdAttribute(null,ad,ad.getCategory().getAttributes().get(), fakerValue);
+  }
 }
