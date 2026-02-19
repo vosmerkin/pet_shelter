81d27900d7d9e10feba648036f95b652191d2bd1
Yevhen Melnyk <vosmerkin.evgen@gmail.com>
2026-02-19 20:39:28 +0200

    fixed userbyemail endpoint

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,15 @@ docker login -u vosmerkin
 docker build -f xDockerfile.txt -t vosmerkin/pet_shelter:latest .
 docker push vosmerkin/pet_shelter:latest
 
-echo copy files to suse
-scp target/marketplace-0.0.1-SNAPSHOT.jar evgen@192.168.31.61:/home/evgen/pet_shelter
+#echo copy files to suse
+#scp target/marketplace-0.0.1-SNAPSHOT.jar evgen@192.168.31.61:/home/evgen/pet_shelter
 
 echo ssh to suse
 ssh evgen@192.168.31.61 << EOF
   cd pet_shelter
 
-  echo docker postgress
+  echo docker compose up
+  docker compose pull
   docker compose up -d
   #if [ ! "$(docker ps -a -q -f name=postgres_db_marketplace)" ]; then
   #    if [ "$(docker ps -aq -f status=exited -f name=postgres_db_marketplace)" ]; then
@@ -22,12 +23,12 @@ ssh evgen@192.168.31.61 << EOF
   #    docker run -d --name <name> my-docker-image
   #fi
 
-  echo run app
+  #echo run app
   #pkill -f marketplace-0.0.1-SNAPSHOT.jar || echo "No running Java app found, continuing..."
   #nohup java -jar -Dspring.profiles.active=prod marketplace-0.0.1-SNAPSHOT.jar > app.log 2>&1 &
 
   source env_vars.sh
-  source rerun_in_normal.sh
+  #source rerun_in_normal.sh
 
   echo "✅ Deployment complete! App is running in the background."
 EOF
